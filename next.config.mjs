4cdfc59896b5e286d280a446d9c1df8e54c973ba
Luiz <valentimelo12@gmail.com>
2026-06-09 15:19:01 -0300

    fix: bundle database file for SSR pages in Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ const nextConfig = {
   experimental: {
     outputFileTracingIncludes: {
       '/api/**/*': ['./data/**/*'],
+      '/jogar': ['./data/**/*'],
+      '/duelo/**/*': ['./data/**/*'],
+      '/*': ['./data/**/*']
     },
     serverComponentsExternalPackages: ['better-sqlite3'],
   },
