02368105051c613d79b1f1210bfcb77c3ee83b88
Luiz <valentimelo12@gmail.com>
2026-06-10 11:13:55 -0300

    fix(auth): Redirecionar para a pagina atual apos o login

--- lib/useAuth.ts
+++ lib/useAuth.ts
@@ -41,7 +41,7 @@ export function useAuth() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: typeof window !== 'undefined' ? `${window.location.origin}/jogar` : undefined
+        redirectTo: typeof window !== 'undefined' ? window.location.href : undefined
       }
     });
     if (error) console.error('Error with Google Login', error);
